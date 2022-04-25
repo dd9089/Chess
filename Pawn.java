1327a0aff9871db31ccc36ee0f2fdccd9751b82b
Chase Balmer <66214702+chaserwacer@users.noreply.github.com>
2022-04-25 09:03:42 -0400

    Add files via upload

---
+++ Pawn.java
@@ -0,0 +1,30 @@
+//Pawn chess piece subclass
+//Made by Chase on 4/22
+
+public class Pawn extends chessPiece
+{
+	//Variables
+	private String pieceName = "Pawn";
+
+
+	//Constructor
+	public Pawn(String c, int x, int y)
+	{
+		super(c, x, y);
+	}
+
+
+	//Methods
+	public String getPieceName()
+	{
+		//Obtain type of piece
+		return pieceName;
+	}
+
+	public void move()
+	{
+
+	}
+
+
+}//end of class
