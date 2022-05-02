e9e7e3afc19178a4592cfd09d234c52169f1365a
Keith-The-Guy <96588547+Keith-The-Guy@users.noreply.github.com>
2022-05-02 12:35:33 -0400

    added checkSpotValid

--- Knight.java
+++ Knight.java
@@ -25,6 +25,41 @@ public class Knight extends chessPiece
 	{
 
 	}
+	
+	//Is valid move method
+	public boolean checkSpotValid(int goalX, int goalY, Board b1)
+	{
+		//Initial tests
+		if(goalX == super.getXLocation() && goalY == super.getYLocation())
+		{
+			//return false if move method attempts to move to the current location
+			return false;
+		}
+
+		if(goalX < 0 && goalX > 7 || goalY < 0 || goalY > 7)
+		{
+			//return false if coords not on board
+			return false;
+		}
+
+		//check if there is a piece, if friendly or enemy
+		//note that there is no code checking if there is a piece inbetween the piece and the locaton because the knight can jump over pieces
+		if(b1[goalX][goalY].getHasPiece())
+		{
+			if(b1[goalX][goalY].getPColor().equalsIgnoreCase(super.getPieceColor()))
+				return false;
+			else
+				return true;
+		}
+		else
+		{
+			return true;	//return true if there is no piece in the spot we are trying to move to
+		}
+
+
+		//return false if coord was not a valid location to move
+		return false;
+	}//end is valid move
 
 
 }//end of class
