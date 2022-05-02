597b300f58311068e7f83fc3e0a79afbfc019e62
Keith-The-Guy <96588547+Keith-The-Guy@users.noreply.github.com>
2022-05-02 12:43:17 -0400

    added checkSpotValid

--- King.java
+++ King.java
@@ -26,5 +26,45 @@ public class King extends chessPiece
 
 	}
 
+	//Is valid move method
+	public boolean checkSpotValid(int goalX, int goalY, Board b1)
+	{
+		//Initial tests
+			if(goalX == super.getXLocation() || goalY == super.getYLocation())
+			{
+				//return false if move method attempts to move to the current location
+				return false;
+			}
+
+			if(goalX < 0 && goalX > 7 || goalY < 0 || goalY > 7)
+			{
+				//return false if coords not on board
+				return false;
+			}
+			
+			//checks if the piece is within range
+			if(Math.abs(goalX - super.getXLocation()) > 1 || Math.abs(goalY - super.getYLocation()) > 1)
+			{
+				//return false if coords are not within one space away
+				return false;
+			}
+
+			//check if there is a piece, if friendly or enemy
+			if(b1[goalX][goalY].getHasPiece())
+			{
+				if(b1[goalX][goalY].getPColor().equalsIgnoreCase(super.getPieceColor()))
+					return false;
+				else
+					return true;
+			}
+			else
+			{
+				return true;	//return true if there is no piece in the spot we are trying to move to
+			}
+
+
+		//return false if coord was not a valid location to move
+		return false;
+	}//end is valid move
 
 }//end of class
