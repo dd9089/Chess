1b71eedd4579138cad50f1eaa3ca4e50c3eeb73a
Thunderball006 <103469439+Thunderball006@users.noreply.github.com>
2022-05-03 09:06:39 -0400

    Update Rook.java
    Made into different sections/ worked out logic for seeing if the move is valid. Just need to know how to use the given coords to move the piece.

--- Rook.java
+++ Rook.java
@@ -33,8 +33,20 @@ public class Rook extends chessPiece
 
 	public void move(int x, int y)
 	{
+		int f = potentialForward(x ,y);
+		int b = potentialBackward(x, y);
+		int r = potentialRight(x, y);
+		int l = potentialLeft(x, y);
+		
+		if(x == r || x == l)
+			System.out.println("The move is valid");
+		else if (y == f || y == b)
+			System.out.println("The move is valid");
+		else
+			System.out.println("The move is not valid");
 
 	}
+	//th int intx and y are the goal values to be input
 	//method to obtain location that the rook can't move to forward
 	public int potentialForward (int x, int y) {
 		
@@ -43,26 +55,27 @@ public class Rook extends chessPiece
 		int xGoal = x;
 		int xGoal = y;
 		boolean validMove = true;
-		int badMoveF = yValue;
+		int goodMoveF = yValue + 1;
 
 		//portion to make sure the goals are actually on the board.
 		boolean good = true;
-		if (!(xGoal >= 0 && xGoal <= 7))
+		if (!(xGoal >= 0 && xGoal <= 7)) {
 			good = false;
-		for (int f = yValue; f <= yGoal; f++) {
-			if (b1[xValue][f].getHasPiece())
-				badMoveF = f;
-		}//end of for
-		return badMoveF;
+			for (int f = yValue; f <= yGoal; f++) {
+				if (!(b1[xValue][f].getHasPiece()))
+				goodMoveF = f;
+			}//end of for
+		}
+		return goodMoveF;
 	}
 	//method to obtain location that the rook can't move to back
-	public int potentialBackwards(int x, int y) {
+	public int potentialBackward(int x, int y) {
 		int xValue = getXLocation();				//getXLocation();         //should theorectically have a 0-7 for now, can change later to fit needs
 		int yValue = getYLOcation();				//getYLocation();
 		int xGoal = x;
 		int xGoal = y;
 		boolean validMove = true;
-		int badMoveB = yValue;
+		int goodMoveB = yValue;
 
 
 		//portion to make sure the goals are actually on the board.
@@ -70,116 +83,57 @@ public class Rook extends chessPiece
 		if (!(xGoal >= 0 && xGoal <= 7))
 			good = false;
 			
-		for (int b = yValue; b >= yGoal ; b--) {
+			for (int b = yValue; b >= yGoal ; b--) {
 			if (b1{[xValue][b].getHasPiece())
-				badMoveF = b;
-		}//end of for
+					goodMoveB = b;
+			}//end of for
+		}//end of if
+		return goodMoveB;
 	}
 	//method to obtain location that the rook can't move to right
 	public int potentialRight (int x, int y) {
-	
-		for (int r = xValue; r <= xGoal; r++){
-			if (b1[r][yValue].getHasPiece())
-				badMoveR = r;
-		}//end of for
-	}
-	//method to obtin location that the rook can't move to left
-	public int potentialBack(int x, int y) {
-		
-	for (int l = xValue; l >= xGoal; l--) {
-		if (b1[r][yValue].getHasPiece())
-			badMoveL = l;
-	}//end of for
-	}
-	//takes in the goal values of the x and y coordiantes.
-	public int potentialMoves(int x, int y)
-	{
 		int xValue = getXLocation();				//getXLocation();         //should theorectically have a 0-7 for now, can change later to fit needs
 		int yValue = getYLOcation();				//getYLocation();
 		int xGoal = x;
 		int xGoal = y;
 		boolean validMove = true;
-		int badMoveF = yValue;
-		int badMoveB = yValue;
-		int badMOveR = xValue;
-		int badMoveL = xValue;
+		int goodMoveR = xValue;
+
+
+		//portion to make sure the goals are actually on the board.
+		boolean good = true;
+		if (!(xGoal >= 0 && xGoal <= 7)) {
+			good = false;
+	
+			for (int r = xValue; r <= xGoal; r++){
+				if (b1[r][yValue].getHasPiece())
+					goodMoveR = r;
+			}//end of for
+		}//end of if
+		return goodMoveR
+	}
+	//method to obtin location that the rook can't move to left
+	public int potentiaLeft(int x, int y) {
+		int xValue = getXLocation();				//getXLocation();         //should theorectically have a 0-7 for now, can change later to fit needs
+		int yValue = getYLOcation();				//getYLocation();
+		int xGoal = x;
+		int xGoal = y;
+		boolean validMove = true;
+		int goodMoveL = xValue;
+
 
 		//portion to make sure the goals are actually on the board.
 		boolean good = true;
 		if (!(xGoal >= 0 && xGoal <= 7))
 			good = false;
-
-
-
 		
-
-		/*
-		//movement for white rook
-		if (getPieceColor().equals("white") {
-			//moving forward
-			for (int f = yValue; f <= yGoal; f++) {
-				if (b1[xValue][f].getHasPiece())
-					badMoveF = f;
-			}//end of for
-
-
-			//moving backwards
-			for (int b = yValue; b >= yGoal ; b--) {
-				if (b1{[xValue][b].getHasPiece())
-					badMoveF = b;
-			}//end of for
-
-
-			//moving to the right
-			for (int r = xValue; r <= xGoal; r++){
-				if (b1[r][yValue].getHasPiece())
-					badMoveR = r;
-			}//end of for
-
-			//moving to the left
 			for (int l = xValue; l >= xGoal; l--) {
 				if (b1[r][yValue].getHasPiece())
-					badMoveL = l;
+					goodMoveL = l;
 			}//end of for
-		}
-
-
-
-
-
-
-
-		//movement for black rook
-		if (getPieceColor().equals("black") {
-			//moving forward
-			for (int f = yValue; f <= yGoal; f++) {
-				if (b1[xValue][f].getHasPiece())
-					badMoveF = f;
-			}//end of for
-
-
-			//moving backwards
-			for (int b = yValue; b >= yGoal ; b--) {
-				if (b1{[xValue][b].getHasPiece())
-					badMoveb = b;
-			}//end of for
-
-
-			//moving to the right
-			for (int r = xValue; r <= xGoal; r++){
-				if (b1[r][yValue].getHasPiece())
-					badMoveR = r;
-			}//end of for
-
-			//moving to the left
-			for (int l = xValue; l >= xGoal; l--) {
-				if (b1[r][yValue].getHasPiece())
-					badMoveL = l;
-			}//end of for
-		}
-		*/
-
-
+		}//end of if
+		return goodMoveL;
 	}
 
+	
 }//end class
